e401df2760888cd3f198fe684aa5eea06c72d4a9
khairaf <khairanifitratul@gmail.com>
2019-06-25 23:47:03 +0700

    channel

---
+++ channel.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func foo(c chan int, someValue int){
+	c <- someValue*5 //add hasil perkalian tersebut ke channel c
+}
+
+func main (){
+	fooVal := make(chan int)
+
+	go foo(fooVal, 5)
+	go foo(fooVal, 3)
+
+	v1, v2 := <- fooVal, <- fooVal
+	fmt.Println(v1, v2)
+}
+
+// <- adalah channel operator
