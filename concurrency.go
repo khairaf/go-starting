08bc9da843382dadf956ff3017cca1bf12d3927d
khairaf <khairanifitratul@gmail.com>
2019-06-25 23:20:58 +0700

    defer

--- concurrency.go
+++ concurrency.go
@@ -3,17 +3,43 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func say(s string) {
+	defer wg.Done()
 	for i:=0; i < 3; i++ {
 		time.Sleep(100*time.Millisecond)
 		fmt.Println(s)
 	}	
+	//wg.Done()
+}
+
+
+func main() {
+	wg.Add(1)
+	go say("Hey")
+	wg.Add(1)
+	say("there you go")
+	//time.Sleep(1000*time.Millisecond)
+	
+
+	wg.Add(1)
+	go say("Buzz")
+	wg.Wait()
+}
+
+/*package main
+
+import "fmt"
+
+func foo() {
+  defer fmt.Println("Done!")
+  fmt.Println("Doing some stuff, who knows what?")
 }
 
 func main() {
-	go say("Hey")
-	say("there")
-	time.Sleep(1000*time.Millisecond)
-}
+  foo()
+}*/
